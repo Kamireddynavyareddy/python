cd7650da6329d714989b0a5ef58826011f6d05b1
Kamireddynavyareddy <navyareddy3777@gmail.com>
2025-04-21 12:37:49 +0530

    python

---
+++ json/task1.py
@@ -0,0 +1,12 @@
+import requests
+#extract data from jsonplaceholder typicode.com
+data = requests.get('https://jsonplaceholder.typicode.com/users')
+users=data.json()
+#print(type(data))
+#print(type(data.json()))
+#print(users)
+new_users=[]
+for user in users:
+    new_users.append((user['id'],user['name']))
+
+print(new_users)
